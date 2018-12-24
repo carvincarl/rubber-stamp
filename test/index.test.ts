282e958436f6690609eb49e07ce297577c394ab7
carl <carvincarl@gmail.com>
2018-12-24 14:03:05 -0600

    cleaned test comments

--- test/index.test.ts
+++ test/index.test.ts
@@ -27,11 +27,6 @@ describe('My Probot app', () => {
   })
 })
 
-// For more information about testing with Jest see:
 // https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
 // https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
 // https://github.com/nock/nock
